b0b8a7b4f9b220c586aeaf4842bfa16dbfe191f1
Guilherme Pereira <guilherme.pereira@insidesistemas.com.br>
2026-03-26 17:48:22 -0300

    feat: add vercel deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CampeonatoModule } from './api/campeonato/campeonato.module';
 import { GoogleSheetService } from './infra/google-sheet/google-sheet.service';
+import { CacheModule } from "@nestjs/cache-manager";
 
+@Module({
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 120000, // 2 min em ms
+    }),
+    CampeonatoModule,
+  ],
+})
 @Module({
   imports: [CampeonatoModule],
   providers: [GoogleSheetService],
